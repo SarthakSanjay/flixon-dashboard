881ddd6657fadc94caff0668af41a005d0fbf7ce
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-04-23 22:20:29 +0530

    feat: add content component

---
+++ src/components/Content.tsx
@@ -0,0 +1,27 @@
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { SidebarTrigger } from "./ui/sidebar";
+
+export default function Content({ type }: { type: string }) {
+  let contentType = type.charAt(0).toUpperCase() + type.slice(1);
+  return (
+    <div className="h-full w-full mx-auto">
+      <div className="h-10 w-full flex items-center">
+        <SidebarTrigger />
+      </div>
+      <div className="">
+        <div className="h-max w-full flex items-center justify-between px-10">
+          <h1 className="">{contentType}s</h1>
+          <Button className=" bg-green-400 text-black hover:bg-green-700">
+            Insert New {contentType}
+          </Button>
+        </div>
+
+        <div className="w-1/3 flex ml-10 gap-2">
+          <Input placeholder={`Search ${type}s`} />
+          <Button>Search</Button>
+        </div>
+      </div>
+    </div>
+  );
+}
