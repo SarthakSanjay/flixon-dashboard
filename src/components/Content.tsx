9d98661341eb1a61882752451694e4a134a3fad1
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-04-25 23:52:28 +0530

    add: navgation on click insert btn

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import { useState } from "react";
 import { Button } from "./ui/button";
-import { Input } from "./ui/input";
 import { SidebarTrigger } from "./ui/sidebar";
 import { SearchBox } from "./SearchBox";
+import { useRouter } from "next/navigation";
 
 export default function Content({ type }: { type: string }) {
   let contentType = type.charAt(0).toUpperCase() + type.slice(1);
+  const router = useRouter();
 
   return (
     <div className="h-full w-full mx-auto">
@@ -17,7 +17,10 @@ export default function Content({ type }: { type: string }) {
       <div className="">
         <div className="h-max w-full flex items-center justify-between px-10">
           <h1 className="">{contentType}s</h1>
-          <Button className=" bg-green-400 text-black hover:bg-green-700">
+          <Button
+            className=" bg-green-400 text-black hover:bg-green-700"
+            onClick={() => router.push(`/${type}/insert`)}
+          >
             Insert New {contentType}
           </Button>
         </div>
