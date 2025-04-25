42e6d6da5215fbd7ebccba203985dd8fe0c7c322
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-04-25 23:51:36 +0530

    add: constant

---
+++ src/app/constants/constant.ts
@@ -0,0 +1,27 @@
+export const GENRE = [
+  "action",
+  "comedy",
+  "thriller",
+  "horror",
+  "drama",
+  "sci-fi",
+  "romance",
+  "fantasy",
+  "crime",
+  "animation",
+];
+
+export const LANGUAGE = [
+  "English",
+  "Hindi",
+  "Japanese",
+  "Telgu",
+  "Tamil",
+  "Kannada",
+  "French",
+  "Mandarin",
+  "Spanish",
+  "Arabic",
+  "Portuguese",
+  "Korean",
+];
