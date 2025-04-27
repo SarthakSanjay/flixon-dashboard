7d302bf93a8e1ffa1dd388561d96bad063f513ea
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-04-27 21:29:47 +0530

    add more atoms

--- src/app/atoms/atom.ts
+++ src/app/atoms/atom.ts
@@ -3,3 +3,5 @@ import { atom } from "jotai";
 export const genreAtom = atom<string[]>([]);
 export const languageAtom = atom<string[]>([]);
 export const subtitleAtom = atom<string[]>([]);
+export const currentProgressAtom = atom(1);
+export const selectedCastAtom = atom<{ image: string; name: string }[]>([]);
