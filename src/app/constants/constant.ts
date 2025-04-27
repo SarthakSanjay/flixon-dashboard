8ce79ceed10aede5f8b4f8b8f78df8c41d206fa2
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-04-27 21:33:34 +0530

    update: add age rating array

--- src/app/constants/constant.ts
+++ src/app/constants/constant.ts
@@ -25,3 +25,20 @@ export const LANGUAGE = [
   "Portuguese",
   "Korean",
 ];
+
+export const AGE_RATINGS = [
+  // Movies (MPAA - US)
+  "G", // General Audiences
+  "PG", // Parental Guidance Suggested
+  "PG-13", // Parents Strongly Cautioned
+  "R", // Restricted
+  "NC-17", // Adults Only
+
+  // TV Shows (TV Parental Guidelines - US)
+  "TV-Y", // All Children
+  "TV-Y7", // Directed to Older Children
+  "TV-G", // General Audience
+  "TV-PG", // Parental Guidance Suggested
+  "TV-14", // Parents Strongly Cautioned
+  "TV-MA", // Mature Audience Only
+];
