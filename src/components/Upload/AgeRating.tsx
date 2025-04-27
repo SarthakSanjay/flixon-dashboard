af6f44c9bf2543b6f65df78bf1a26f8e56600d16
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-04-27 21:24:26 +0530

    feat: add age rating component

---
+++ src/components/Upload/AgeRating.tsx
@@ -0,0 +1,37 @@
+import { AGE_RATINGS } from "@/app/constants/constant";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Label } from "../ui/label";
+import { FormLabel } from "../ui/form";
+
+export function AgeRating() {
+  return (
+    <div className="h-max w-max space-y-2">
+      <FormLabel>Age Rating</FormLabel>
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Select a fruit" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Age Rating</SelectLabel>
+            {AGE_RATINGS.map((rating, index) => {
+              return (
+                <SelectItem key={index + rating} value={rating}>
+                  {rating}
+                </SelectItem>
+              );
+            })}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+}
