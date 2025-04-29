68cb29e6fc96bbef06bb07c651837d78eef1faac
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-04-29 22:49:12 +0530

    fix: now first letter of each word in a sentence will be capitalized

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function capitalizeFirstLetter(word: string) {
+  let newWord = word.split(" ").map((w) => {
+    return w.charAt(0).toUpperCase() + w.slice(1);
+  });
+  return newWord.join(" ");
 }
